64d88dc1412d1c3a6eeb00c6ea930c219f76fa0e
YevgenBoiko <yevheniiboiko@gmail.com>
2023-07-08 13:52:53 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <GlobalStyles />
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/final-project-frontend-goose">
+        <BrowserRouter basename="/GooseTrack">
           <App />
         </BrowserRouter>
       </PersistGate>
